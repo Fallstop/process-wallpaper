48a2745007a5e1bb5a1f65f146d0e6bb97038b74
Jasper M-W <jasper@ngita.co.nz>
2020-11-29 21:52:25 +1300

    Automation go brrrrr

--- updateWallpaper.sh
+++ updateWallpaper.sh
@@ -9,4 +9,4 @@ nice python3 generateWallpaper.py
 echo "Updating Desktop"
 WALLPAPER_PATH="$/home/fallstop/Documents/projects/process-wallpaper/wallpaper.png"
 
-qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript 'var allDesktops = desktops(); print (allDesktops);for (i=0;i<allDesktops.length;i++) {d = allDesktops[i];d.wallpaperPlugin = "org.kde.slideshow";d.currentConfigGroup = Array("Wallpaper", "org.kde.slideshow", "General");d.writeConfig("SlidePaths", "'${WALLPAPER_PATH}'")}'
+
